0f01860bbf290464de66c60a9c160e9a8565b735
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2022-01-01 20:59:15 +0530

    Enter

---
+++ db.js
@@ -0,0 +1,11 @@
+const moongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+
+const connectToMongo = ()=>{
+    moongoose.connect(mongoURI,()=>{
+        console.log("Connected to Mongo successfully");
+    })
+}
+
+module.exports = connectToMongo;
