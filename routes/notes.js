6d25706c5f403cd1d2704ac4938502775ea9f025
shivam singh <92594066+Shivamgits@users.noreply.github.com>
2022-01-05 01:08:02 +0530

    Notes adding ,updating and deleting

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,121 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator"); //validator so that no can save empty notes
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
 
+//ROUTE 1:get lAll the Notes using: GET "/api/notes/fetchallnotes". Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+    // catch is used when surver is down 
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-router.get('/',  (req, res)=>{
-    obj = {
-        a:'theta',
-        number:34
-    };
-    res.json(obj)
+//ROUTE 2:add a new  Note using: POST "/api/notes/addnote". Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body(
+      "description",
+      "description must be atleast 5 characters long"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { description, title, tag } = req.body;
+      // If there are errors, return Bad request and the errors
 
-})
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
+      const note = new Note({
+        //return promise
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save(); //this function will wait until save notes and then it will return
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
+//ROUTE 3: Updating a existing  Note using: PUT "/api/notes/updatenote". Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { description, title, tag } = req.body;
+  try {
+    // If there are errors, return Bad request and the errors
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // create a new Note object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.title = description;
+    }
+    if (tag) {
+      newNote.tags = tag;
+    }
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      res.status(404).send("Not Found");
+    }
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    ); // new : true is for new contact create
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//ROUTE 4: Deleting a existing  Note using: DELETE "/api/notes/deletenote". Login required
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  
+  try {
+    //Find the note to be deleted and delete it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      res.status(404).send("Not Found");
+    }
+
+    //Allow deletion only if user owns this note
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = router;
